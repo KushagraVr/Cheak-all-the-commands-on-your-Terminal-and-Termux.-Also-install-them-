1dcae71aa59f4b8ae07c9fd447e0683144c44314
Kushagra Verma <147381889+KushagraVr@users.noreply.github.com>
2024-03-13 17:56:10 +0530

    main.sh

--- main.sh
+++ main.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-echo "Enter the command"
-read -r name
+echo "Are you using a terminal or Termux?"
+echo "1 = Terminal"
+echo "2 = Termux"
 
-command="/usr/bin/$name"
+read -p "Enter your choice: " choice
 
-if [ -x "$command" ]
-then 
-    echo "Available"
+if [ "$choice" == "1" ]; then
+    echo "Enter the command:"
+    read -r name
+
+    command="/usr/bin/$name"
+
+    if [ -x "$command" ]; then
+        echo "Available"
+    else
+        echo "Installing"
+        sudo apt update -y && sudo apt install "$name"
+        "$name"
+    fi
+
+elif [ "$choice" == "2" ]; then
+    echo "Enter the command:"
+    read -r name
+
+    if command -v "$name" &> /dev/null; then
+        echo "Available"
+    else
+        echo "Installing"
+        pkg update && pkg install "$name"
+        "$name"
+    fi
 else
-    echo "Installing"
-    sudo apt update -y && sudo apt install "$name"
-htop
+    echo "Invalid choice."
 fi
+
